20c5c48b361c2de5b1d16112e4e74c6244b26afa
Gustav Karlström <gustav.karlstrom96@hotmail.com>
2018-02-23 09:45:11 +0100

    Comments

--- userClass.js
+++ userClass.js
@@ -2,13 +2,13 @@ class User {
   constructor() {
       this.image = undefined; // Fetch image from facebook objekt
       this.name = undefined; // Fetch name from facebook objekt
-      this.coins = undefined; // Fetch coins from database if null add 5000 as start
-      this.rank = undefined; // Fetch rank from database if null set 0;
-      this.wins = undefined; // Fetch wins from database if null set 0;
-      this.loses = undefined; // Fetch wins from database if null set 0;
+      this.coins = undefined; //fetch from db || 5000;
+      this.rank = undefined; // Fetch rank from database || 0;
+      this.wins = undefined; // Fetch wins from database || 0;
+      this.loses = undefined; // Fetch wins from database || 0;
       this.totBets = this.wins + this.loses;
       this.winPercentage = this.wins / this.totBets;
-      this.bettingLimit = 1000;
+      this.bettingLimit = // Fetch bettinglimit from db || 1000;
     },
     logout() {
       // Logout the user;
@@ -21,6 +21,7 @@ class User {
         alert("Your name can't be longer then 30chars nor be less then 3. Are you even a String ?");
       } else {
         this.name = name;
+        // save to db;
       }
     },
     changeLimit(limit){
@@ -28,6 +29,7 @@ class User {
         alert("Limit you must, between the length of 6 and 2 a Number should do.")
       } else {
         this.bettingLimit = limit;
+        // save to db;
       }
     }
 
